3458a596a383143ebfde2e6abee29b423c351077
denis <denis@localhost>
2012-07-28 20:53:40 -0600

    Some good bug fixes.

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -86,7 +86,7 @@ else{
    }
    Grid.convert();
    Grid.printGrid1();
-  if (Grid.Grid[4] == Grid.Grid[5] && Grid.Grid [5] == Grid.Grid[6] && Grid.Grid[5] != 0)// need to come with more clever way
+  if (Grid.Grid[4] == Grid.Grid[5] && Grid.Grid [5] == Grid.Grid[6] && Grid.Grid[5] != 0)// need to come up with with more clever way
   {
       Grid.gameRun = false;
   }
