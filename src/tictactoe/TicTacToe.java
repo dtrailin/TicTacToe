0aa767564a6f45287d9ec7c6bfcd31f6dc9f8726
denis <denis@localhost>
2012-08-01 18:23:21 -0600

    Added step 2 AI.

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -159,52 +159,94 @@ public class TicTacToe {
                 boolean AIwork = true;
                 while (AIwork == true) {
                     for (int x = 0; x <= 6; x += 2) {
-                        if (Grid[x + 1] == Grid[x + 2] && Grid[x + 2] == 2 && Grid[x + 3] == 0) {
+                        if (Grid[x + 1] == Grid[x + 2] && Grid[x + 2] == 2 && Grid[x + 3] == 0  && AIwork == true) {
                             Grid[x + 3] = 2;
                             AIwork = false;
-                        } else if (Grid[x + 2] == Grid[x + 3] && Grid[x + 3] == 2 && Grid[x + 1] == 0) {
+                        } else if (Grid[x + 2] == Grid[x + 3] && Grid[x + 3] == 2 && Grid[x + 1] == 0 && AIwork == true) {
                             Grid[x + 1] = 2;
                             AIwork = false;
-                        } else if (Grid[x + 1] == Grid[x + 3] && Grid[x + 3] == 2 && Grid[x + 2] == 0) {
+                        } else if (Grid[x + 1] == Grid[x + 3] && Grid[x + 3] == 2 && Grid[x + 2] == 0 && AIwork == true) {
                             Grid[x + 2] = 2;
                             AIwork = false;
                         }
                         for (int l = 0; l < 3; l++) {
-                            if (Grid[l + 1] == Grid[l + 4] && Grid[l + 4] == 2 && Grid[l + 7] == 0) {
+                             if (Grid[l + 1] == Grid[l + 4] && Grid[l + 4] == 2 && Grid[l + 7] == 0 && AIwork == true) {
                                 Grid[l + 7] = 2;
                                 AIwork = false;
-                            } else if (Grid[l + 4] == Grid[l + 7] && Grid[l + 4] == 2 && Grid[l + 1] == 0) {
+                            } else if (Grid[l + 4] == Grid[l + 7] && Grid[l + 4] == 2 && Grid[l + 1] == 0 && AIwork == true) {
                                 Grid[l + 1] = 2;
                                 AIwork = false;
-                            } else if (Grid[l + 1] == Grid[l + 7] && Grid[l + 1] == 2 && Grid[l + 4] == 0) {
+                            } else if (Grid[l + 1] == Grid[l + 7] && Grid[l + 1] == 2 && Grid[l + 4] == 0 && AIwork == true) {
                                 Grid[l + 4] = 2;
                                 AIwork = false;
                             }
                         }
-                        if (Grid[1] == Grid[5] && Grid[5] == 2 && Grid[9] == 0) {
+                        if (Grid[1] == Grid[5] && Grid[5] == 2 && Grid[9] == 0 && AIwork == true) {
                             Grid[9] = 2;
                             AIwork = false;
-                        } else if (Grid[5] == Grid[9] && Grid[5] == 2 && Grid[1] == 0) {
+                        } else if (Grid[5] == Grid[9] && Grid[5] == 2 && Grid[1] == 0 && AIwork == true) {
                             Grid[1] = 2;
                             AIwork = false;
-                        } else if (Grid[1] == Grid[9] && Grid[9] == 2 && Grid[5] == 0) {
+                        } else if (Grid[1] == Grid[9] && Grid[9] == 2 && Grid[5] == 0 && AIwork == true) {
                             Grid[5] = 2;
                             AIwork = false;
-                        } else if (Grid[3] == Grid[5] && Grid[5] == 2 && Grid[7] == 0) {
+                        } else if (Grid[3] == Grid[5] && Grid[5] == 2 && Grid[7] == 0 && AIwork == true) {
                             Grid[7] = 2;
                             AIwork = false;
-                        } else if (Grid[3] == Grid[7] && Grid[3] == 2 && Grid[5] == 0) {
+                        } else if (Grid[3] == Grid[7] && Grid[3] == 2 && Grid[5] == 0 && AIwork == true) {
                             Grid[5] = 2;
                             AIwork = false;
-                        } else if (Grid[5] == Grid[7] && Grid[5] == 2 && Grid[3] == 0) {
+                        } else if (Grid[5] == Grid[7] && Grid[5] == 2 && Grid[3] == 0 && AIwork == true) {
+                            Grid[3] = 2;
+                            AIwork = false;
+                        }
+                    }
+                          for ( int x = 0; x <= 6; x += 2) {
+                        if (Grid[x + 1] == Grid[x + 2] && Grid[x + 2] == 1 && Grid[x + 3] == 0  && AIwork == true) {
+                            Grid[x + 3] = 2;
+                            AIwork = false;
+                        } else if (Grid[x + 2] == Grid[x + 3] && Grid[x + 3] == 1 && Grid[x + 1] == 0 && AIwork == true) {
+                            Grid[x + 1] = 2;
+                            AIwork = false;
+                        } else if (Grid[x + 1] == Grid[x + 3] && Grid[x + 3] == 1 && Grid[x + 2] == 0 && AIwork == true) {
+                            Grid[x + 2] = 2;
+                            AIwork = false;
+                        }
+                        for (int l = 0; l < 3; l++) {
+                             if (Grid[l + 1] == Grid[l + 4] && Grid[l + 4] == 1 && Grid[l + 7] == 0 && AIwork == true) {
+                                Grid[l + 7] = 2;
+                                AIwork = false;
+                            } else if (Grid[l + 4] == Grid[l + 7] && Grid[l + 4] == 1 && Grid[l + 1] == 0 && AIwork == true) {
+                                Grid[l + 1] = 2;
+                                AIwork = false;
+                            } else if (Grid[l + 1] == Grid[l + 7] && Grid[l + 1] == 1 && Grid[l + 4] == 0 && AIwork == true) {
+                                Grid[l + 4] = 2;
+                                AIwork = false;
+                            }
+                        }
+                        if (Grid[1] == Grid[5] && Grid[5] == 1 && Grid[9] == 0 && AIwork == true) {
+                            Grid[9] = 2;
+                            AIwork = false;
+                        } else if (Grid[5] == Grid[9] && Grid[5] == 1 && Grid[1] == 0 && AIwork == true) {
+                            Grid[1] = 2;
+                            AIwork = false;
+                        } else if (Grid[1] == Grid[9] && Grid[9] == 1 && Grid[5] == 0 && AIwork == true) {
+                            Grid[5] = 2;
+                            AIwork = false;
+                        } else if (Grid[3] == Grid[5] && Grid[5] == 1 && Grid[7] == 0 && AIwork == true) {
+                            Grid[7] = 2;
+                            AIwork = false;
+                        } else if (Grid[3] == Grid[7] && Grid[3] == 1 && Grid[5] == 0 && AIwork == true) {
+                            Grid[5] = 2;
+                            AIwork = false;
+                        } else if (Grid[5] == Grid[7] && Grid[5] == 1 && Grid[3] == 0 && AIwork == true) {
                             Grid[3] = 2;
                             AIwork = false;
                         }
                     }
                     int AIplace = 1 + rand.nextInt(9);
-                    if (Grid[AIplace] == 0) {
+                    if (Grid[AIplace] == 0  && AIwork == true) {
                         Grid[AIplace] = 2;
-                        //System.out.println("1");
                         AIwork = false;
                     }
                 }
